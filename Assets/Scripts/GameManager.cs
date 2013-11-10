c6038b317671ff9f120f62523346e69de714c01f
arouton <arouton@gmail.com>
2013-11-09 23:17:29 -0500

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,251 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject m_MarkerPrefab;
+	GameObject m_Marker;
+	public Dictionary<Vector2, Hexagon> m_Map = new Dictionary<Vector2, Hexagon>();
+	
+	List<int> m_NextHexagonIds = new List<int>();
+	public List<Hexagon> m_NextHexagons = new List<Hexagon>();
+	
+	public Hexagon m_BackgroundHexagonPrefab;
+	
+	public TextMesh m_ScoreTextMesh;
+	int m_Score;
+	
+	void Start ()
+	{
+		m_Marker = GameObject.Instantiate(m_MarkerPrefab) as GameObject;
+		
+		m_CurrentPosition = Vector2.zero;
+		
+		m_Marker.transform.position = _AxialCoordinatesTo3DCoordinates(m_CurrentPosition) + Vector3.up;
+		
+		for(int i = -2; i<=2; ++i)
+		{
+			for(int j = Mathf.Max(-2, -i-2); j <= Mathf.Min(2, -i+2); ++j)
+			{
+				Vector3 position = _AxialCoordinatesTo3DCoordinates(new Vector2(i, -i-j));
+				GameObject.Instantiate(m_BackgroundHexagonPrefab, position - Vector3.up, Quaternion.identity);
+			}
+		}
+		
+		m_NextHexagonIds.Add(Random.Range(0, m_HexagonPrefabs.Count));
+		m_NextHexagonIds.Add(Random.Range(0, m_HexagonPrefabs.Count));
+		m_NextHexagonIds.Add(Random.Range(0, m_HexagonPrefabs.Count));
+		FixNextHexagonMaterials();
+		
+		CreateHexagon(Vector2.zero);
+		
+		m_ScoreTextMesh.text = m_Score.ToString();
+	}
+	
+	int m_HexId = 0;
+	Hexagon CreateHexagon(Vector2 a_AxialCoordinates)
+	{
+		int index = m_NextHexagonIds[0];
+		
+		Vector3 position = _AxialCoordinatesTo3DCoordinates(a_AxialCoordinates);
+		Hexagon hex = GameObject.Instantiate(m_HexagonPrefabs[index], position, Quaternion.identity) as Hexagon;
+		hex.m_Coordinates = a_AxialCoordinates;
+		hex.m_GameManager = this;
+		hex.m_Id = index;
+		hex.name += (" " + m_HexId);
+		++m_HexId;
+		m_Map[a_AxialCoordinates] = hex;
+		
+		m_NextHexagonIds[0] = m_NextHexagonIds[1]; 
+		m_NextHexagonIds[1] = m_NextHexagonIds[2]; 
+		m_NextHexagonIds[2] = Random.Range(0, m_HexagonPrefabs.Count);
+		FixNextHexagonMaterials();
+		
+		return hex;
+	}
+
+	void DestroyHexagon(Hexagon a_Hexagon)
+	{
+		m_Map.Remove(a_Hexagon.m_Coordinates);
+		Destroy(a_Hexagon.gameObject);
+	}
+	
+	Vector2 m_BeginningMousePosition = Vector2.zero;
+	bool m_IsSwipeActive = false;
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+			m_BeginningMousePosition = mousePosition;
+			m_IsSwipeActive = true;
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			if (m_IsSwipeActive)
+			{
+				m_IsSwipeActive = false;
+				Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				Vector2 delta = mousePosition - m_BeginningMousePosition;
+				if (delta.magnitude > 50f)
+				{
+					m_IsSwipeActive = false;
+					SwipeTo(delta);
+				}
+			}
+		}
+		if (Input.GetMouseButton(0))
+		{
+			if (m_IsSwipeActive)
+			{
+				Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				Vector2 delta = mousePosition - m_BeginningMousePosition;
+				if (delta.magnitude > 50f)
+				{
+					m_IsSwipeActive = false;
+					SwipeTo(delta);
+				}
+			}
+		}
+		
+		// List of possible moves
+		bool validMove = false;
+		foreach(Vector2 direction in GetNeighborCoordinates())
+		{
+			Vector2 coordinate = m_CurrentPosition + direction;
+			if(!m_Map.ContainsKey(coordinate) && (GetDistanceBetween(Vector2.zero, coordinate)<=2f))
+			{
+				validMove = true;
+				break;
+			}
+		}
+		if (!validMove)
+		{
+			Application.LoadLevel(Application.loadedLevelName);
+		}
+	}
+	
+	public List<Hexagon> m_HexagonPrefabs = new List<Hexagon>();
+	Vector2 m_CurrentPosition = Vector2.zero;
+	void SwipeTo(Vector2 a_Delta)
+	{
+		Vector2 newPosition = m_CurrentPosition + _InputDeltaToDisplacement(a_Delta);		
+		
+		if (m_Map.ContainsKey(newPosition)) return;	
+		if (GetDistanceBetween(Vector2.zero, newPosition) > 2f) return;
+		
+		m_Score += 5;
+		
+		m_CurrentPosition = newPosition;	
+		m_Marker.transform.position = _AxialCoordinatesTo3DCoordinates(m_CurrentPosition) + Vector3.up;	
+		Hexagon hex = CreateHexagon(m_CurrentPosition);
+		
+//		List<Hexagon> sameColorHexagons = new List<Hexagon>();
+//		RecursivelyGetSameColorHexagons(hex, ref sameColorHexagons);
+//		if (sameColorHexagons.Count > 2)
+//		{
+//			m_Score += 10 * (int)(Mathf.Pow(2, (sameColorHexagons.Count-2)));
+//			foreach(Hexagon sameColorHexagon in sameColorHexagons)
+//			{
+//				if (sameColorHexagon != hex)
+//				{
+//					DestroyHexagon(sameColorHexagon);
+//				}
+//			}
+//		}
+		
+		m_ScoreTextMesh.text = m_Score.ToString();
+	}
+	
+	[HideInInspector]
+	public Vector2 m_AxialAxis2 = new Vector2(0, 1);
+	[HideInInspector]
+	public Vector2 m_AxialAxis0 = new Vector2(1, 0);
+	[HideInInspector]
+	public Vector2 m_AxialAxis1 = new Vector2(1, -1);
+	
+	Vector2 _InputDeltaToDisplacement(Vector2 a_InputDelta)
+	{
+		Vector2 axis0 = new Vector2(0f, 1f);
+		float dot0 = Vector2.Dot(axis0, a_InputDelta);
+		Vector2 axis1 = new Vector2(Mathf.Cos(Mathf.PI / 6f), Mathf.Sin(Mathf.PI / 6f));
+		float dot1 = Vector2.Dot(axis1, a_InputDelta);
+		Vector2 axis2 = new Vector2(Mathf.Cos(5f * Mathf.PI / 6f), Mathf.Sin(5f * Mathf.PI / 6f));
+		float dot2 = Vector2.Dot(axis2, a_InputDelta);
+		
+
+		if (Mathf.Abs(dot0) > Mathf.Abs(dot1))
+		{
+			if (Mathf.Abs(dot0) > Mathf.Abs(dot2))
+			{
+				return (dot0 > 0f) ? m_AxialAxis0 : -m_AxialAxis0;
+			}
+			else
+			{
+				return (dot2 > 0f) ? m_AxialAxis1 : -m_AxialAxis1;				
+			}
+		}
+		else
+		{
+			if (Mathf.Abs(dot1) > Mathf.Abs(dot2))
+			{
+				return (dot1 > 0f) ? m_AxialAxis2 : -m_AxialAxis2;	
+			}
+			else
+			{
+				return (dot2 > 0f) ? m_AxialAxis1 : -m_AxialAxis1;				
+			}	
+		}
+	}
+	
+	Vector2 _AxialCoordinatesToCoordinates(Vector2 a_AxialCoordinates)
+	{
+		float size = 1;
+		return size * new Vector2(3f/2f * a_AxialCoordinates.y, Mathf.Sqrt(3) * (a_AxialCoordinates.x + a_AxialCoordinates.y/2f));
+	}
+	
+	Vector3 _AxialCoordinatesTo3DCoordinates(Vector2 a_AxialCoordinates)
+	{
+		float size = 1;
+		return size * new Vector3(3f/2f * a_AxialCoordinates.y, 0f, Mathf.Sqrt(3) * (a_AxialCoordinates.x + a_AxialCoordinates.y/2f));
+	}
+	
+	void RecursivelyGetSameColorHexagons(Hexagon a_Hexagon, ref List<Hexagon> a_List)
+	{
+		foreach(Hexagon hex in a_Hexagon.GetNeighbors())
+		{
+			if ((hex.m_Id == a_Hexagon.m_Id) && (!a_List.Contains(hex)))
+			{
+				a_List.Add(hex);
+				RecursivelyGetSameColorHexagons(hex, ref a_List);
+			}
+		}
+	}
+	
+	void FixNextHexagonMaterials()
+	{
+		for(int i = 0; i < m_NextHexagons.Count; ++i)
+		{
+			Material m = m_HexagonPrefabs[m_NextHexagonIds[i]].GetComponent<Renderer>().sharedMaterial;
+			m_NextHexagons[i].GetComponent<Renderer>().material = m;
+		}
+	}
+	
+	public float GetDistanceBetween(Vector2 a_Coordinates1, Vector2 a_Coordinates2)
+	{
+	    return (Mathf.Abs(a_Coordinates1.x - a_Coordinates2.x) +
+			+ Mathf.Abs(a_Coordinates1.y - a_Coordinates2.y)
+			+ Mathf.Abs(a_Coordinates1.x + a_Coordinates1.y - a_Coordinates2.x - a_Coordinates2.y)) / 2f;
+	}
+	
+	public IEnumerable GetNeighborCoordinates()
+	{
+		yield return -m_AxialAxis0;
+		yield return m_AxialAxis0;
+		yield return -m_AxialAxis1;
+		yield return m_AxialAxis1;
+		yield return -m_AxialAxis2;
+		yield return m_AxialAxis2;
+	}
+}
---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hexagon : MonoBehaviour
+{
+	MeshFilter m_MeshFilter;
+	
+	public float m_Height = 1f;
+	public float m_Radius = 1f;
+	
+	public GameManager m_GameManager;
+	
+	public int m_Id;
+
+	void Start ()
+	{
+		m_MeshFilter = GetComponent<MeshFilter>();
+		Mesh mesh = new Mesh();
+		
+		List<Vector3> vertices = new List<Vector3>();
+		List<Vector3> normals = new List<Vector3>();
+		List<Vector2> uvs = new List<Vector2>();
+		
+		// Top
+		vertices.Add(new Vector3(0f, 0.5f * m_Height, 0f));
+		normals.Add(Vector3.up);
+		uvs.Add(Vector2.zero);
+		for(int i=0; i<6; ++i)
+		{
+			float angle = i / 6f * 2f * Mathf.PI;
+			vertices.Add(new Vector3(m_Radius * Mathf.Cos(angle), 0.5f * m_Height, m_Radius * Mathf.Sin(angle)));
+			normals.Add(Vector3.up);
+			uvs.Add(new Vector2(m_Radius * Mathf.Cos(angle), m_Radius * Mathf.Sin(angle)));
+		}
+		// Bottom
+		vertices.Add(new Vector3(0f, -0.5f * m_Height, 0f));
+		normals.Add(Vector3.down);
+		uvs.Add(Vector2.zero);
+		for(int i=0; i<6; ++i)
+		{
+			float angle = i / 6f * 2f * Mathf.PI;
+			vertices.Add(new Vector3(m_Radius * Mathf.Cos(angle), -0.5f * m_Height, m_Radius * Mathf.Sin(angle)));
+			normals.Add (Vector3.down);
+			uvs.Add(new Vector2(m_Radius * Mathf.Cos(angle), m_Radius * Mathf.Sin(angle)));
+		}
+		// Size
+		for(int i=0; i<6; ++i)
+		{
+			float angle = i / 6f * 2f * Mathf.PI;
+			vertices.Add(new Vector3(m_Radius * Mathf.Cos(angle), 0.5f * m_Height, m_Radius * Mathf.Sin(angle)));
+			normals.Add(new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)));
+			uvs.Add(new Vector2(m_Radius * Mathf.Cos(angle), m_Radius * Mathf.Sin(angle)));
+		}
+		for(int i=0; i<6; ++i)
+		{
+			float angle = i / 6f * 2f * Mathf.PI;
+			vertices.Add(new Vector3(m_Radius * Mathf.Cos(angle), -0.5f * m_Height, m_Radius * Mathf.Sin(angle)));
+			normals.Add(new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)));
+			uvs.Add(new Vector2(m_Radius * Mathf.Cos(angle), m_Radius * Mathf.Sin(angle)));
+		}
+		
+		List<int> indices = new List<int>();
+		for(int i=1; i<6; ++i)
+		{
+			indices.Add(0);
+			indices.Add(i+1);
+			indices.Add(i);
+		}
+		indices.Add(0);
+		indices.Add(1);
+		indices.Add(6);
+
+		for(int i=8; i<13; ++i)
+		{
+			indices.Add(7);
+			indices.Add(i);
+			indices.Add(i+1);
+		}
+		indices.Add(7);
+		indices.Add(13);
+		indices.Add(8);
+		
+		for(int i=14; i<19; ++i)
+		{
+			indices.Add(i);
+			indices.Add(i+1);
+			indices.Add(i+6);
+		}
+		indices.Add(19);
+		indices.Add(14);
+		indices.Add(25);
+		for(int i=14; i<19; ++i)
+		{
+			indices.Add(i+1);
+			indices.Add(i+7);
+			indices.Add(i+6);
+		}
+		indices.Add(14);
+		indices.Add(20);
+		indices.Add(25);
+		
+		mesh.Clear();
+		mesh.vertices = vertices.ToArray();
+		mesh.normals = normals.ToArray();
+		mesh.uv = uvs.ToArray();
+		mesh.triangles = indices.ToArray();
+		m_MeshFilter.mesh = mesh;
+	}
+	
+	
+	public Vector2 m_Coordinates = Vector2.zero;
+	
+	public IEnumerable GetNeighbors()
+	{
+		var map = m_GameManager.m_Map;
+		if (map.ContainsKey(m_Coordinates-m_GameManager.m_AxialAxis0))
+		{
+			yield return map[m_Coordinates-m_GameManager.m_AxialAxis0];
+		}
+		if (map.ContainsKey(m_Coordinates+m_GameManager.m_AxialAxis0))
+		{
+			yield return map[m_Coordinates+m_GameManager.m_AxialAxis0];
+		}
+		if (map.ContainsKey(m_Coordinates-m_GameManager.m_AxialAxis1))
+		{
+			yield return map[m_Coordinates-m_GameManager.m_AxialAxis1];
+		}
+		if (map.ContainsKey(m_Coordinates+m_GameManager.m_AxialAxis1))
+		{
+			yield return map[m_Coordinates+m_GameManager.m_AxialAxis1];
+		}
+		if (map.ContainsKey(m_Coordinates-m_GameManager.m_AxialAxis2))
+		{
+			yield return map[m_Coordinates-m_GameManager.m_AxialAxis2];
+		}
+		if (map.ContainsKey(m_Coordinates+m_GameManager.m_AxialAxis2))
+		{
+			yield return map[m_Coordinates+m_GameManager.m_AxialAxis2];
+		}
+	}
+}
